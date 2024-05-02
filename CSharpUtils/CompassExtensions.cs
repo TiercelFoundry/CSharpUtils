7a0ca9c19189bcc0c4086c37760b365a4ca84fa0
Grahame Watt <grahame@eyrus.com>
2024-05-02 16:09:47 -0700

    additional compass extensions

--- CSharpUtils/CompassExtensions.cs
+++ CSharpUtils/CompassExtensions.cs
@@ -4,7 +4,7 @@ public static class CompassExtensions
 {
     public static Compass Clockwise(this Compass current, int steps = 1) 
     {
-        return (Compass)((int)(current + steps) % 8);
+        return (Compass)(Math.Abs((int)(current + steps) % 8));
     }
 
     public static Compass Clockwise(this Compass current) {
@@ -13,11 +13,26 @@ public static class CompassExtensions
 
     public static Compass Anticlockwise(this Compass current, int steps = 1) 
     {
-        return (Compass)((int)(current - steps) % 8);
+        return (Compass)(Math.Abs((int)(current - steps) % 8));
     }
 
     public static Compass Opposite(this Compass current)
     {
         return current < Compass.S ? current + 4 : current - 4;
+    }
+
+    public static int ClockwiseStepsTo(this Compass start, Compass end)
+    {
+        return end < start ? (int)(Compass.NW - start - end) : end - start;
+    }
+
+    public static int AnticlockwiseStepsTo(this Compass start, Compass end)
+    {
+        return 8 - start.ClockwiseStepsTo(end);
+    }
+
+    public static bool IsCardinal (this Compass compass)
+    {
+        return ((int)compass& 1) == 0;
     }
 }
