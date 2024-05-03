d4d13ac6fb99634ca7b798a34a98a3b2ec36438b
Grahame Watt <grahame@eyrus.com>
2024-05-03 16:47:32 -0700

    bitcompass navigation fixes

---
+++ CSharpUtils.Tests/BitCompassTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+
+namespace TiercelFoundry.CSharpUtils.Tests;
+
+public class BitCompassTests
+{
+    [Test]
+    public void BitCompassClockwiseTests()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(BitCompass.N.Clockwise(), Is.EqualTo(BitCompass.NE));
+            Assert.That(BitCompass.NE.Clockwise(), Is.EqualTo(BitCompass.E));
+            Assert.That(BitCompass.E.Clockwise(), Is.EqualTo(BitCompass.SE));
+            Assert.That(BitCompass.SE.Clockwise(), Is.EqualTo(BitCompass.S));
+            Assert.That(BitCompass.S.Clockwise(), Is.EqualTo(BitCompass.SW));
+            Assert.That(BitCompass.SW.Clockwise(), Is.EqualTo(BitCompass.W));
+            Assert.That(BitCompass.W.Clockwise(), Is.EqualTo(BitCompass.NW));
+            Assert.That(BitCompass.NW.Clockwise(), Is.EqualTo(BitCompass.N));
+
+            Assert.That(BitCompass.N.Clockwise(4), Is.EqualTo(BitCompass.S));
+            Assert.That(BitCompass.NE.Clockwise(4), Is.EqualTo(BitCompass.SW));
+            Assert.That(BitCompass.E.Clockwise(4), Is.EqualTo(BitCompass.W));
+            Assert.That(BitCompass.SE.Clockwise(4), Is.EqualTo(BitCompass.NW));
+            Assert.That(BitCompass.S.Clockwise(4), Is.EqualTo(BitCompass.N));
+            Assert.That(BitCompass.SW.Clockwise(4), Is.EqualTo(BitCompass.NE));
+            Assert.That(BitCompass.W.Clockwise(4), Is.EqualTo(BitCompass.E));
+            Assert.That(BitCompass.NW.Clockwise(4), Is.EqualTo(BitCompass.SE));
+        });
+    }
+
+    [Test]
+    public void BitCompassAnticlockwiseTests()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(BitCompass.N.Anticlockwise(), Is.EqualTo(BitCompass.NW));
+            Assert.That(BitCompass.NE.Anticlockwise(), Is.EqualTo(BitCompass.N));
+            Assert.That(BitCompass.E.Anticlockwise(), Is.EqualTo(BitCompass.NE));
+            Assert.That(BitCompass.SE.Anticlockwise(), Is.EqualTo(BitCompass.E));
+            Assert.That(BitCompass.S.Anticlockwise(), Is.EqualTo(BitCompass.SE));
+            Assert.That(BitCompass.SW.Anticlockwise(), Is.EqualTo(BitCompass.S));
+            Assert.That(BitCompass.W.Anticlockwise(), Is.EqualTo(BitCompass.SW));
+            Assert.That(BitCompass.NW.Anticlockwise(), Is.EqualTo(BitCompass.W));
+
+            Assert.That(BitCompass.N.Anticlockwise(4), Is.EqualTo(BitCompass.S));
+            Assert.That(BitCompass.NE.Anticlockwise(4), Is.EqualTo(BitCompass.SW));
+            Assert.That(BitCompass.E.Anticlockwise(4), Is.EqualTo(BitCompass.W));
+            Assert.That(BitCompass.SE.Anticlockwise(4), Is.EqualTo(BitCompass.NW));
+            Assert.That(BitCompass.S.Anticlockwise(4), Is.EqualTo(BitCompass.N));
+            Assert.That(BitCompass.SW.Anticlockwise(4), Is.EqualTo(BitCompass.NE));
+            Assert.That(BitCompass.W.Anticlockwise(4), Is.EqualTo(BitCompass.E));
+            Assert.That(BitCompass.NW.Anticlockwise(4), Is.EqualTo(BitCompass.SE));
+        });
+    }
+
+    [Test]
+    public void BitCompassOppositeTests()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(BitCompass.N.Opposite(), Is.EqualTo(BitCompass.S));
+            Assert.That(BitCompass.NE.Opposite(), Is.EqualTo(BitCompass.SW));
+            Assert.That(BitCompass.E.Opposite(), Is.EqualTo(BitCompass.W));
+            Assert.That(BitCompass.SE.Opposite(), Is.EqualTo(BitCompass.NW));
+            Assert.That(BitCompass.S.Opposite(), Is.EqualTo(BitCompass.N));
+            Assert.That(BitCompass.SW.Opposite(), Is.EqualTo(BitCompass.NE));
+            Assert.That(BitCompass.W.Opposite(), Is.EqualTo(BitCompass.E));
+            Assert.That(BitCompass.NW.Opposite(), Is.EqualTo(BitCompass.SE));
+        });
+    }
+}
--- CSharpUtils/BitCompassExtensions.cs
+++ CSharpUtils/BitCompassExtensions.cs
@@ -5,18 +5,20 @@ public static class BitCompassExtensions
     public static BitCompass Clockwise(this BitCompass current, int steps = 1)
     {
         byte val = (byte)current;
-        return (BitCompass)(val << steps | val >> 8 - steps);
+        var rotated = (byte)((val << steps) | (val >> (8 - steps)));
+        return (BitCompass)(unchecked(rotated)); // mask because byte and c#: https://stackoverflow.com/questions/737781/left-bit-shifting-255-as-a-byte
     }
 
     public static BitCompass Anticlockwise(this BitCompass current, int steps = 1)
     {
         byte val = (byte)current;
-        return (BitCompass)(val >> steps | val << 8 - steps);
+        var rotated = (byte)((val >> steps) | (val << (8 - steps)));
+        return (BitCompass)(unchecked(rotated)); // mask because byte and c#: https://stackoverflow.com/questions/737781/left-bit-shifting-255-as-a-byte
     }
 
     public static BitCompass Opposite(this BitCompass current)
     {
         byte val = (byte)current;
-        return (BitCompass)(val << 4 | val >> 4);
+        return (BitCompass)(val > 8 ? val >> 4 : val << 4);
     }
 }
