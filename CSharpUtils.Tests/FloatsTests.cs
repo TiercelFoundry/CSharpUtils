84ce81eb2d82e79fbbfccf4dc239cdea4fd1ce95
Grahame Watt <watt.grah@gmail.com>
2024-07-01 17:40:13 -0700

    tests floats conversions

---
+++ CSharpUtils.Tests/FloatsTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace TiercelFoundry.CSharpUtils.Tests;
+
+public class FloatsTests
+{
+    [Test]
+    public void RadianDegreeConstants()
+    {
+        Assert.Multiple(() =>
+        {
+            Assert.That(Floats.Deg2Rad, Is.InRange(0.01745f, 0.01746f));
+            Assert.That(Floats.Rad2Deg, Is.InRange(57.2957f, 57.2958f));
+        });
+    }
+}
