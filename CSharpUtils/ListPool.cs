48d02122de41675d3157ea83b5265837f0758d8a
Grahame Watt <watt.grah@gmail.com>
2024-07-25 14:17:55 -0700

    list pool

---
+++ CSharpUtils/ListPool.cs
@@ -0,0 +1,17 @@
+﻿namespace TiercelFoundry.CSharpUtils;
+
+public static class ListPool<T>
+{
+    private readonly static Stack<List<T>> stack = new();
+
+    public static List<T> Get()
+    {
+        return stack.Count > 0 ? stack.Pop() : new List<T>();
+    }
+
+    public static void Add(List<T> list)
+    {
+        list.Clear();
+        stack.Push(list);
+    }
+}
