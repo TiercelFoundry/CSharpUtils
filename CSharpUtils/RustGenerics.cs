779654060fb608b66627c2a0efdf07a8f628d80f
Grahame Watt <watt.grah@gmail.com>
2024-08-19 16:05:24 -0700

    rust generics

---
+++ CSharpUtils/RustGenerics.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace TiercelFoundry.CSharpUtils.Rusty;
+
+public static class RustGenerics
+{
+    public static T Expect<T>([NotNull] this T? obj, string message) where T : class
+    {
+        if (obj == null)
+        {
+            throw new NullReferenceException(message);
+        }
+
+        return obj;
+    }
+}
