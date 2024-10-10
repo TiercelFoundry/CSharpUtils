d3a4496b01518f88ed1d0aaf254d9e0ad4bc8b87
Grahame Watt <watt.grah@gmail.com>
2024-10-10 13:55:02 -0700

    empty actions

---
+++ CSharpUtils/Actions.cs
@@ -0,0 +1,9 @@
+﻿namespace TiercelFoundry.CSharpUtils;
+
+public static class Actions
+{
+    public static void Empty() { }
+    public static void Empty<T>(T _) { }
+    public static void Empty<T1, T2>(T1 _1, T2 _2) { }
+    /* Put as many overloads as you want */
+}
---
+++ CSharpUtils/Functions.cs
@@ -0,0 +1,18 @@
+﻿namespace TiercelFoundry.CSharpUtils;
+
+public static class Functions
+{
+    public static T Identity<T>(T value) { return value; }
+
+    public static T0 Default<T0>() { return default(T0); }
+    public static T0 Default<T1, T0>(T1 _) { return default(T0); }
+    /* Put as many overloads as you want */
+
+    /* Some other potential methods */
+    public static bool IsNull<T>(T entity) where T : class { return entity == null; }
+    public static bool IsNonNull<T>(T entity) where T : class { return entity != null; }
+
+    /* Put as many overloads for True and False as you want */
+    public static bool True<T>(T _) { return true; }
+    public static bool False<T>(T _) { return false; }
+}
